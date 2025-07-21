3fcc29daa0bf0c1527ae97695e33327da91d7a1c
lanatheniqabi <mahnurhabib519@gmail.com>
2025-07-21 20:45:18 +0500

    Update first_prog.c

--- compiling-my-demons/first_prog.c
+++ compiling-my-demons/first_prog.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() 
+{
+    int x;
+
+    printf("What are your grades? "); 
+    scanf("%d", &x);        
+
+    // The if-else block must be INSIDE the main function
+    if (x > 5)
+    {
+        printf("you pass!\n");
+    }
+    else 
+    {
+        printf("you fail :(\n");
+    }
+
+    return 0;
+}
